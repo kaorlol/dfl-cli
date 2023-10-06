cd9ad2e4af4a496d16af5b2f7d68a42fbf3693c4
Kaoru <jacobyb0508@gmail.com>
2023-10-06 14:40:36 -0400

    cleaned ig

--- src/elapsed.rs
+++ src/elapsed.rs
@@ -1,12 +1,14 @@
 use std::time::Instant;
 
+const TIMES: [u128; 3] = [1000, 60000, 3600000];
+
 pub fn get_elapsed_time(time: Instant) -> String {
     let elapsed = time.elapsed().as_millis();
     let formatted_elapsed = match elapsed {
         0..=999 => format!("{}ms", elapsed),
-        1000..=59999 => format!("{}s", elapsed / 1000),
-        60000..=3599999 => format!("{}m {}s", elapsed / 60000, (elapsed % 60000) / 1000),
-        _ => format!("{}h {}m", elapsed / 3600000, (elapsed % 3600000) / 60000)
+        1000..=59999 => format!("{}s", elapsed / TIMES[0]),
+        60000..=3599999 => format!("{}m {}s", elapsed / TIMES[1], (elapsed % TIMES[1]) / TIMES[0]),
+        _ => format!("{}h {}m", elapsed / TIMES[2], (elapsed % TIMES[2]) / TIMES[1])
     };
 
     return formatted_elapsed;
